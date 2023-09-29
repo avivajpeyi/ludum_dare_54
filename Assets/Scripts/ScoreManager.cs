e39fc8a610333f50c46eb4cc837a6bddea2704ac
Kieran <syber_kierandavidson@hotmail.com>
2023-09-30 12:29:42 +1300

    kihiojf

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -6,6 +6,7 @@ using TMPro;
 public class ScoreManager : MonoBehaviour
 {
     // [SerializeField] private Text Score;
+    
     // Start is called before the first frame update
     void Start()
     {
