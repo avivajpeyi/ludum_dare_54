eeb7a28bb8210ca650f8da3bf6fc4e42c08175af
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2023-10-01 21:55:23 +1300

    add start hacking

--- Assets/StartGameButton.cs
+++ Assets/StartGameButton.cs
@@ -3,20 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class StartGameButton : MonoBehaviour
 {
-    public string nextSceneName; // Name of the next scene to load
 
     public void StartGame()
     {
-        // Check if the scene name is provided
-        if (!string.IsNullOrEmpty(nextSceneName))
-        {
-            // Load the specified scene
-            SceneManager.LoadScene(nextSceneName);
-        }
-        else
-        {
-            // Load the next scene (assuming the game scene is the next one)
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
+        SceneManager.LoadScene("FinalScene");
     }
 }
